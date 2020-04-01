fd658d3b0c5176b8c100999d8085d23b007ac5cb
Aditio Agung Nugroho <aditioagungnugroho@Aditios-MacBook-Pro.local>
2020-04-01 19:20:20 +1300

    Create login template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="home">
+        <h1 className="title-app">Partifys</h1>
+
+        <p className="highlight">A premium Spotify account is required to use Partifys</p>
+
+        <input type="text" name="username" className="input-login icon-key" placeholder="Party code" />
+        <button type="button" className="button-join">Join Party</button>
+        <button type="button" className="button-login">Create Party</button>
+      </div>
     </div>
   );
 }
